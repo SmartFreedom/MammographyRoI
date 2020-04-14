363d9c5bdbd94c47436c915fc540f798dbf1a08e
vessemer <vessemer@gmail.com>
2020-04-14 20:36:43 +0300

    dataset update

--- src/modules/dataset.py
+++ src/modules/dataset.py
@@ -37,6 +37,45 @@ def collater(data):
     return collated
 
 
+def infer_on_batch(model, data):
+    batch = [data['image']]
+    if np.array(data['shape']).max() > 1000:
+        batch = [ torch.unsqueeze(d, 0) for d in data['image'] ]
+
+    torch.cuda.empty_cache()
+    predict = list()
+    for image in batch:
+        image = torch.autograd.Variable(image).cuda()
+        predict.append(model(image).sigmoid().data.cpu().numpy())
+        image = image.data.cpu().numpy()
+        torch.cuda.empty_cache()
+    predict = np.squeeze(np.stack(predict))
+    return predict if len(predict.shape) == 4 else np.squeeze(predict, axis=0)
+
+
+def inference_collater(data):
+    pids = [ pid for s in data for pid in [ s['pid']] * 8 ]
+    shapes = [ pid for s in data for pid in [ s['shape']] * 8 ]
+    imgs = [ s['image'] for s in data ]
+    _shapes = np.array([ s.shape[1:] for s in imgs ])
+    max_shape = _shapes.max() 
+    max_shape += (64 - max_shape % 64) * (max_shape % 64 != 0)
+
+    imgs = [np.pad(
+        s, 
+        ((0, 0),
+        (0, max_shape - s.shape[1]),
+        (0, max_shape - s.shape[2])),
+        mode='constant',
+        constant_values=np.median(s)
+    ) for s in imgs ]
+    imgs = torch.stack([ 
+        torch.Tensor(im.copy())
+        for s in imgs for im in augs._rotate_mirror_do(s) ])
+    
+    return { 'image': imgs, 'pid': pids, 'shape': shapes }
+
+
 def stack_aug_collater(data):
     collated = easydict.EasyDict()
     
@@ -49,14 +88,17 @@ def stack_aug_collater(data):
             np.max([a['mask'], b['mask']], axis=0)
             for a, b in zip(data[::2], data[1::2])
         ])
-    ).unsqueeze(1)
+    )
+    if len(collated.mask.shape) == 3:
+        collated.mask = collated.mask.unsqueeze(1)
     collated.pid = [s['pid'] for s in data]
 
     return collated
 
 
 class SegmentationDataset(Dataset):
-    def __init__(self, data: pd.DataFrame, root=config.PATHS.EXPERIMENT_DATA, augmentations=None):
+    def __init__(self, data: pd.DataFrame, root=config.PATHS.EXPERIMENT_DATA, augmentations=None, with_centroid=False):
+        self.with_centroid = with_centroid
         self.augmentations = augmentations
         self.data = data.copy()
         self.groups = data.groupby('fileid').groups
@@ -86,6 +128,8 @@ class SegmentationDataset(Dataset):
         pad = 32 - data.image.shape[1] % 32 if data.image.shape[1] % 32 else 0
         image = np.pad(data.image, (0, pad), mode='constant')
         data.mask = np.pad(data.mask, (0, pad), mode='constant')
+        if len(data.mask.shape) == 3:
+            data.mask = np.rollaxis(data.mask, 2, 0)
         data.image = img_transform(np.expand_dims(image, -1))
 
         return data
@@ -95,12 +139,54 @@ class SegmentationDataset(Dataset):
 
     def load_annotations(self, fileid):
         fileid = fileid.split('.')
-        fileid[-2] = fileid[-2] + '_RoI_mask'
+        fileid[-2] = fileid[-2] + '_{}'
         fileid = '.'.join(fileid)
-        return cv2.imread(os.path.join(self.root, fileid), 0)
-
+        mask = list()
+        mask = [
+            cv2.imread(
+                os.path.join(self.root, fileid.format('segmentaciya-tolshya-zhelez')), 0),
+            cv2.imread(
+                os.path.join(self.root, fileid.format('segmentaciya-zhelez')), 0)]
+        cgs = cv2.imread(
+                os.path.join(self.root, fileid.format('centroid-grudnogo-soska')), 0)
+        if self.with_centroid:
+            mask.append(cgs if cgs is not None else np.zeros_like(mask[0]))
+        if mask[0].shape != mask[-1].shape:
+            print(fileid)
+            print(mask[0].shape, mask[1].shape, mask[-1].shape)
+        
+        return np.dstack(mask)
+    
     def num_classes(self):
         return 2
+
+    def __len__(self):
+        return len(self.keys)
+
+
+class InferenceDataset(Dataset):
+    def __init__(self, keys, root=config.PATHS.EXPERIMENT_DATA):
+        # key == case/fileid
+        self.keys = keys
+        self.root = root
+
+    def __getitem__(self, idx):
+        fileid = self.keys[idx]
+        image = self.load_image(fileid)
+        data = { 
+            "image": image, 
+            "pid": fileid, 
+            "shape": image.shape 
+        }
+        return self.postprocess(data)
+
+    def postprocess(self, data):
+        data = easydict.EasyDict(data)
+        data.image = img_transform(np.expand_dims(data.image, -1))
+        return data
+
+    def load_image(self, fileid):
+        return cv2.imread(os.path.join(self.root, fileid), 0)
 
     def __len__(self):
         return len(self.keys)
